2392f6aeddd29d211a987e6b943ef41aee2c22e5
Nino Maruszewski <76668284+MrAwesomeRocks@users.noreply.github.com>
2021-06-18 18:07:50 -0500

    Refactor the tiny-remapper CLI (#6)
    * Ignore VSCode files
    
    * Add Picocli
    
    * Start annotating fields
    
    * Tabs ---> spaces
    
    * Improve version detection
    
    * Annotate all switches
    
    * Cleanup
    
    * Annotate parameters
    
    * Fix classpath setter
    
    * Add validation and runner method
    
    * Make all instance variables private
    
    * Add main runner script
    
    * `javax.validation` is not included by default
    
    * Fix version detection
    
    * Fix thread setter
    
    * Version detection again and spec field
    
    * Remove duplicate stuff
    
    - Version and help is already provided
    - Parameters are required by default
    
    * Make a fat jar
    
    * Fix typo
    
    * Fix colors on Windows
    
    * Fat jar replaces application plugin
    
    * Improve the version field
    
    Also made the `CommandSpec` private
    
    * Add a footer and abbreviate the synopsis
    
    * Remove the old verification stuff
    
    * Add man page generation
    
    * Add some short switches
    
    * Booleans are false by default
    
    * Add `v` in front of version (i.e. `ASM v9.1`)
    
    * Use blossom ffor version field
    
    * Format build.dependsOn
    
    * Debugging pt. 1
    
    * Debugging pt. 2
    
    * Couple of Javadocs
    
    * Gradle be quiet
    
    * Make class final, use ROOT locale
    
    * Spaces ---> tabs
    
    * Spaces --> tabs, fix the version in META-INF
    
    * Single to double quotes
    
    * Add comment back in
    
    * Change description of switches with options
    
    * Add docs for Github pages
    
    * Add javadoc folder
    
    * Add Github Action for docs
    
    * Add gitattributes

--- src/main/java/net/fabricmc/tinyremapper/Main.java
+++ src/main/java/net/fabricmc/tinyremapper/Main.java
@@ -31,150 +31,141 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 import net.fabricmc.tinyremapper.TinyRemapper.LinkedMethodPropagation;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Spec;
+import picocli.jansi.graalvm.AnsiConsole;
 
-public class Main {
-	public static void main(String[] rawArgs) {
-		List<String> args = new ArrayList<String>(rawArgs.length);
-		boolean reverse = false;
-		boolean ignoreFieldDesc = false;
-		boolean propagatePrivate = false;
-		LinkedMethodPropagation propagateBridges = LinkedMethodPropagation.DISABLED;
-		boolean removeFrames = false;
-		Set<String> forcePropagation = Collections.emptySet();
-		File forcePropagationFile = null;
-		boolean ignoreConflicts = false;
-		boolean checkPackageAccess = false;
-		boolean fixPackageAccess = false;
-		boolean resolveMissing = false;
-		boolean rebuildSourceFilenames = false;
-		boolean skipLocalVariableMapping = false;
-		boolean renameInvalidLocals = false;
-		NonClassCopyMode ncCopyMode = NonClassCopyMode.FIX_META_INF;
-		int threads = -1;
+/* @formatter:off - eclipse formatter doesn't seem to like annotations */
+/**
+ * The main class for the Tiny-Remapper CLI.
+ */
+@Command(name = "tiny-remapper", mixinStandardHelpOptions = true, version = {
+	"@|bold,underline Tiny Remapper v" + Main.VERSION + "|@",
+	"ASM v" + Main.ASM_VERSION,
+	"Picocli v" + picocli.CommandLine.VERSION,
+	"JVM: ${java.version} (${java.vendor} ${java.vm.name} ${java.vm.version})",
+	"OS: ${os.name} ${os.version} ${os.arch}"},
+	description = "A tool for remapping JAR files using \"Tiny\"-format mappings.",
+	footer = {"%nCopyright (C) 2016, 2018 Player, asie; 2021 QuiltMC",
+		"Report bugs at <https://github.com/QuiltMC/tiny-remapper/issues>."},
+	usageHelpAutoWidth = true, abbreviateSynopsis = true)
+public final class Main implements Callable<Integer> {
+	/* @formatter:on */
 
-		for (String arg : rawArgs) {
-			if (arg.startsWith("--")) {
-				int valueSepPos = arg.indexOf('=');
+	// Version info - done by blossom
+	public static final String VERSION = "__TINY_VERSION";
+	public static final String ASM_VERSION = "__ASM_VERSION";
 
-				String argKey = valueSepPos == -1 ? arg.substring(2) : arg.substring(2, valueSepPos);
-				argKey = argKey.toLowerCase(Locale.ROOT);
+	/*
+	 * ================== PicoCLI stuff =====================
+	 */
+	@Spec
+	private CommandSpec spec; // injected by picocli
 
-				switch (argKey.toLowerCase()) {
-				case "reverse":
-					System.err.println("WARNING: --reverse is not currently implemented!");
-					reverse = true;
-					break;
-				case "ignorefielddesc":
-					ignoreFieldDesc = true;
-					break;
-				case "forcepropagation":
-					forcePropagationFile = new File(arg.substring(valueSepPos + 1));
-					break;
-				case "propagateprivate":
-					propagatePrivate = true;
-					break;
-				case "propagatebridges":
-					switch (arg.substring(valueSepPos + 1).toLowerCase(Locale.ENGLISH)) {
-					case "disabled": propagateBridges = LinkedMethodPropagation.DISABLED; break;
-					case "enabled": propagateBridges = LinkedMethodPropagation.ENABLED; break;
-					case "compatible": propagateBridges = LinkedMethodPropagation.COMPATIBLE; break;
-					default:
-						System.out.println("invalid propagateBridges: "+arg.substring(valueSepPos + 1));
-						System.exit(1);
-					}
+	/*
+	 * ================== Input parameters ==================
+	 */
 
-					break;
-				case "removeframes":
-					removeFrames = true;
-					break;
-				case "ignoreconflicts":
-					ignoreConflicts = true;
-					break;
-				case "checkpackageaccess":
-					checkPackageAccess = true;
-					break;
-				case "fixpackageaccess":
-					fixPackageAccess = true;
-					break;
-				case "resolvemissing":
-					resolveMissing = true;
-					break;
-				case "rebuildsourcefilenames":
-					rebuildSourceFilenames = true;
-					break;
-				case "skiplocalvariablemapping":
-					skipLocalVariableMapping = true;
-					break;
-				case "renameinvalidlocals":
-					renameInvalidLocals = true;
-					break;
-				case "nonclasscopymode":
-					switch (arg.substring(valueSepPos + 1).toLowerCase(Locale.ENGLISH)) {
-					case "unchanged": ncCopyMode = NonClassCopyMode.UNCHANGED; break;
-					case "fixmeta": ncCopyMode = NonClassCopyMode.FIX_META_INF; break;
-					case "skipmeta": ncCopyMode = NonClassCopyMode.SKIP_META_INF; break;
-					default:
-						System.out.println("invalid nonClassCopyMode: "+arg.substring(valueSepPos + 1));
-						System.exit(1);
-					}
+	// Input file
+	private Path inputFile;
 
-					break;
-				case "threads":
-					threads = Integer.parseInt(arg.substring(valueSepPos + 1));
-					if (threads <= 0) {
-						System.out.println("Thread count must be > 0");
-						System.exit(1);
-					}
-					break;
-				default:
-					System.out.println("invalid argument: "+arg+".");
-					System.exit(1);
-				}
-			} else {
-				args.add(arg);
+	/**
+	 * Set the input file path variable and validate.
+	 *
+	 * @param value input path from the user
+	 */
+	@Parameters(index = "0", description = "Path to input file to remap.")
+	private void setInputFile(Path value) {
+		if (!Files.isReadable(value)) {
+			throw new ParameterException(spec.commandLine(),
+					"Cannot read input file " + value + ".");
+		}
+		inputFile = value;
+	}
+
+	@Parameters(index = "1", description = "Path to output remapped file.")
+	private Path outputFile;
+
+	// Mappings file
+	private Path mappings;
+
+	/**
+	 * Set the mappings path variable and validate.
+	 *
+	 * @param value input path from the user
+	 */
+	@Parameters(index = "2", description = "Path to mappings file.")
+	private void setMappings(Path value) {
+		if (!Files.isReadable(value)) {
+			throw new ParameterException(spec.commandLine(),
+					"Cannot read mappings file " + value + ".");
+		}
+		mappings = value;
+	}
+
+	@Parameters(index = "3", description = "Namespace to map from.")
+	private String fromMapping;
+
+	@Parameters(index = "4", description = "Namespace to map to.")
+	private String toMapping;
+
+	// Classpath
+	private Path[] classpath;
+
+	/**
+	 * Set the classpath variable and validate.
+	 *
+	 * @param value input paths from the user
+	 */
+	@Parameters(index = "5..*", description = "Additional files to add to the classpath.")
+	private void setClasspath(Path[] value) {
+		classpath = new Path[value.length];
+		for (int i = 0; i < value.length; i++) {
+			classpath[i] = value[i];
+			if (!Files.isReadable(value[i])) {
+				throw new ParameterException(spec.commandLine(),
+						"Cannot read classpath file " + value[i] + ".");
 			}
 		}
+	}
 
-		if (args.size() < 5) {
-			System.out.println("usage: <input> <output> <mappings> <from> <to> [<classpath>]... [--reverse] [--forcePropagation=<file>] [--propagatePrivate] [--ignoreConflicts]");
-			System.exit(1);
-		}
+	/*
+	 * ================== Options and switches ==============
+	 */
+	@Option(names = {"-R", "--reverse"},
+			description = "Reverse the mapping. @|bold,underline,yellow NOT YET IMPLEMENTED!|@")
+	private boolean reverse;
 
-		Path input = Paths.get(args.get(0));
-		if (!Files.isReadable(input)) {
-			System.out.println("Can't read input file "+input+".");
-			System.exit(1);
-		}
+	@Option(names = {"-i", "--ignore-field-desc"},
+			description = "Ignore the field descriptions in mappings.")
+	private boolean ignoreFieldDesc;
 
-		Path output = Paths.get(args.get(1));
+	// Force propagation option
+	private Set<String> forcePropagation = Collections.emptySet();
 
-		Path mappings = Paths.get(args.get(2));
-		if (!Files.isReadable(mappings) || Files.isDirectory(mappings)) {
-			System.out.println("Can't read mappings file "+mappings+".");
-			System.exit(1);
-		}
-
-		String fromM = args.get(3);
-		String toM = args.get(4);
-
-		Path[] classpath = new Path[args.size() - 5];
-
-		for (int i = 0; i < classpath.length; i++) {
-			classpath[i] = Paths.get(args.get(i + 5));
-			if (!Files.isReadable(classpath[i])) {
-				System.out.println("Can't read classpath file "+i+": "+classpath[i]+".");
-				System.exit(1);
-			}
-		}
-
+	/**
+	 * Set the forcePropagation variable and validate.
+	 *
+	 * @param forcePropagationFile input file from the user
+	 */
+	@Option(names = {"-f", "--force-propagation"},
+			description = "A file with methods to force propagation to.")
+	private void setForcePropagation(File forcePropagationFile) {
 		if (forcePropagationFile != null) {
 			forcePropagation = new HashSet<>();
 
 			if (!forcePropagationFile.canRead()) {
-				System.out.println("Can't read forcePropagation file "+forcePropagationFile+".");
-				System.exit(1);
+				throw new ParameterException(spec.commandLine(),
+						"Cannot read forcePropagation file " + forcePropagationFile + ".");
 			}
 
 			try (BufferedReader reader = new BufferedReader(new FileReader(forcePropagationFile))) {
@@ -183,20 +174,143 @@ public class Main {
 				while ((line = reader.readLine()) != null) {
 					line = line.trim();
 
-					if (line.isEmpty() || line.charAt(0) == '#') continue;
+					if (line.isEmpty() || line.charAt(0) == '#') {
+						continue;
+					}
 
 					forcePropagation.add(line);
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
-				System.exit(1);
+				throw new ParameterException(spec.commandLine(),
+						"An error occurred while parsing the forcePropagation file.");
 			}
+		}
+	}
+
+	@Option(names = {"-p", "--propagate-private"},
+			description = "Propagate mappings to private methods.")
+	private boolean propagatePrivate;
+
+	// Propagate bridges option
+	private LinkedMethodPropagation propagateBridges = LinkedMethodPropagation.DISABLED;
+
+	/**
+	 * Set the propagateBridges variable and validate.
+	 *
+	 * @param value input from the user
+	 */
+	@Option(names = {"-b", "--propagate-bridges"},
+			description = "Propagate methods to bridge methods. "
+					+ "Must be one of the following: \"disabled\", \"enabled\", or \"compatible\".")
+	private void setPropagateBridges(String value) {
+		switch (value.toLowerCase(Locale.ROOT)) {
+			case "disabled":
+				propagateBridges = LinkedMethodPropagation.DISABLED;
+				break;
+			case "enabled":
+				propagateBridges = LinkedMethodPropagation.ENABLED;
+				break;
+			case "compatible":
+				propagateBridges = LinkedMethodPropagation.COMPATIBLE;
+				break;
+			default:
+				throw new ParameterException(spec.commandLine(),
+						"Invalid propagateBridges value: " + value);
+		}
+	}
+
+	@Option(names = "--remove-frames",
+			description = "Ignore the StackMap and StackMapTable frames.")
+	private boolean removeFrames;
+
+	@Option(names = {"-I", "--ignore-conflicts"},
+			description = "Ignore any mapping conflicts.")
+	private boolean ignoreConflicts;
+
+	@Option(names = {"-C", "--check-package-access"},
+			description = "Check package access.")
+	private boolean checkPackageAccess;
+
+	@Option(names = {"-F", "--fix-package-access"},
+			description = "Fix package access. Implies \"--check-package-access\".")
+	private boolean fixPackageAccess;
+
+	@Option(names = {"-m", "--resolve-missing"}, description = "Resolve missing methods.")
+	private boolean resolveMissing;
+
+	@Option(names = {"-r", "--rebuild-source-filenames"},
+			description = "Rebuild the filenames of sources.")
+	private boolean rebuildSourceFilenames;
+
+	@Option(names = {"-l", "--skip-local-variable-mapping"},
+			description = "Skip remapping local variables")
+	private boolean skipLocalVariableMapping;
+
+	@Option(names = {"-L", "--rename-invalid-locals"},
+			description = "Rename invalid local variables.")
+	private boolean renameInvalidLocals;
+
+	// Non-class file copy mode option
+	private NonClassCopyMode ncCopyMode = NonClassCopyMode.FIX_META_INF;
+
+	/**
+	 * Set the setNonClassCopyMode variable and validate.
+	 *
+	 * @param value input from the user
+	 */
+	@Option(names = {"-M", "--non-class-copy-mode"},
+			description = "How to deal with non-class files in a JAR (i.e. META-INF). "
+					+ "Must be one of the following: \"unchanged\", \"fixmeta\", or \"skipmeta\".")
+	private void setNonClassCopyMode(String value) {
+		switch (value.toLowerCase(Locale.ROOT)) {
+			case "unchanged":
+				ncCopyMode = NonClassCopyMode.UNCHANGED;
+				break;
+			case "fixmeta":
+				ncCopyMode = NonClassCopyMode.FIX_META_INF;
+				break;
+			case "skipmeta":
+				ncCopyMode = NonClassCopyMode.SKIP_META_INF;
+				break;
+			default:
+				throw new ParameterException(spec.commandLine(),
+						"Invalid nonClassCopyMode value: " + value);
+		}
+	}
+
+	// Threads option
+	private int threads = -1;
+
+	/**
+	 * Set the threads variable and validate.
+	 *
+	 * @param value
+	 */
+	@Option(names = {"-t", "--threads"},
+			description = "Number of threads to use while remapping. "
+					+ "Defaults to the number of CPU cores available.")
+	private void setThreads(int value) {
+		if (value <= 0) {
+			throw new ParameterException(spec.commandLine(), "Threads must be greater than 0.");
+		}
+		threads = value;
+	}
+
+	/**
+	 * The main function of the CLI.
+	 *
+	 * @return exit code
+	 */
+	public Integer call() throws Exception {
+		if (classpath == null) {
+			classpath = new Path[0]; // PicoCLI makes a null array, Tiny wants an empty one
 		}
 
 		long startTime = System.nanoTime();
 
 		TinyRemapper remapper = TinyRemapper.newRemapper()
-				.withMappings(TinyUtils.createTinyMappingProvider(mappings, fromM, toM))
+				.withMappings(TinyUtils.createTinyMappingProvider(mappings, fromMapping, toMapping))
 				.ignoreFieldDesc(ignoreFieldDesc)
 				.withForcedPropagation(forcePropagation)
 				.propagatePrivate(propagatePrivate)
@@ -212,10 +326,11 @@ public class Main {
 				.threads(threads)
 				.build();
 
-		try (OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(output).build()) {
-			outputConsumer.addNonClassFiles(input, ncCopyMode, remapper);
+		try (OutputConsumerPath outputConsumer =
+				new OutputConsumerPath.Builder(outputFile).build()) {
+			outputConsumer.addNonClassFiles(inputFile, ncCopyMode, remapper);
 
-			remapper.readInputs(input);
+			remapper.readInputs(inputFile);
 			remapper.readClassPath(classpath);
 
 			remapper.apply(outputConsumer);
@@ -226,5 +341,19 @@ public class Main {
 		}
 
 		System.out.printf("Finished after %.2f ms.\n", (System.nanoTime() - startTime) / 1e6);
+		return 0;
+	}
+
+	/**
+	 * Main runner function.
+	 *
+	 * @param args args from the user
+	 */
+	public static void main(String... args) {
+		int exitCode;
+		try (AnsiConsole ansi = AnsiConsole.windowsInstall()) {
+			exitCode = new CommandLine(new Main()).execute(args);
+		}
+		System.exit(exitCode);
 	}
 }
